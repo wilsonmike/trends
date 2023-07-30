e80cd1067842c1665a708fb92d3c68ddd868c559
Michael Wilson <michaelwilsonsj@hotmail.com>
2023-07-30 13:07:10 -0400

    loop for rss feed. test commit

--- main.go
+++ main.go
@@ -9,19 +9,19 @@ import (
 )
 
 type RSS struct {
-  XMLName   xml.Name    `xml:"rss"`  
-  Channel   *Channel    `xml:"channel"`  
+  XMLName   xml.Name    `xml:"rss"`
+  Channel   *Channel    `xml:"channel"`
 }
 
 type Channel struct {
-  Title     string      `xml:"title"` 
-  ItemList  []Item      `xml:"item"` 
+  Title     string      `xml:"title"`
+  ItemList  []Item      `xml:"item"`
 }
 
 type Item struct {
   Title     string      `xml:"title"`
   Link      string      `xml:"link"`
-  Traffic   string      `xml:"approx_traffic"` 
+  Traffic   string      `xml:"approx_traffic"`
   NewsItems []News      `xml:"news_item"`
 }
 
@@ -35,11 +35,19 @@ func main(){
   data := readGoogleTrends()
 
   err := xml.Unmarshal(data, &r)
-  
+
   if err != nil {
     fmt.Println(err)
     os.Exit(1)
   }
+
+  fmt.Println("Trends today,")
+  fmt.Println("--- --- ---")
+  for i := range r.Channel.ItemList {
+      rank := (i + 1)
+  }
+  fmt.Println("#", rank)
+  fmt.Println("Search Term:")
 }
 
 func getGoogleTrends() *http.Response {
