00957549d1597de7f1a6afccb086126583367339
Replit user <>
2023-08-01 02:01:00 +0000

    returning trends in console. testing openai as possible summary feature

--- main.go
+++ main.go
@@ -44,12 +44,14 @@ func main(){
   fmt.Println("Trends today,")
   fmt.Println("--- --- ---")
   for i := range r.Channel.ItemList {
-      rank := (i + 1)
+    rank := (i + 1)
+    fmt.Println("#", rank)
+    fmt.Println("Search Term:", r.Channel.ItemList[i].Title)
+    fmt.Println("Link to the Trend:", r.Channel.ItemList[i].Link)
+    fmt.Println("Headline:", r.Channel.ItemList[i].NewsItems[0].Headline)
+    fmt.Println("Link to Article:", r.Channel.ItemList[i].NewsItems[0].HeadlineLink)
+    fmt.Println("--- --- ---")
   }
-  fmt.Println("#", rank)
-  fmt.Println("Search Term:", r.Channel.ItemList[i].Title)
-  fmt.Println("Link to the Trend:", r.Channel.ItemList[i].Link)
-  fmt.Println("Headline:", r.Channel.ItemList[i].NewsItems[0].Headline)
 }
 
 func getGoogleTrends() *http.Response {
